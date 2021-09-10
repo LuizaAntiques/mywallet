575dd6f8714ea33637bea61a85545b7afea12093
LuizaAntiques <LuizaAntiques@users.noreply.github.com>
2021-09-10 16:10:10 +0000

    deploy: 70bb708864320b2dc7f54991e7ff14139e6d29d0

---
+++ precache-manifest.10768c025e33ddf4c1857e790bb032e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f49d48684ea86d9a9a26bc92023831b",
+    "url": "/mywallet/index.html"
+  },
+  {
+    "revision": "1f41e8638649b6df18cb",
+    "url": "/mywallet/static/css/main.97772349.chunk.css"
+  },
+  {
+    "revision": "a8562bbc4837807d0ed6",
+    "url": "/mywallet/static/js/2.c2493121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mywallet/static/js/2.c2493121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f41e8638649b6df18cb",
+    "url": "/mywallet/static/js/main.10378edf.chunk.js"
+  },
+  {
+    "revision": "5b241b211d784a27ad66",
+    "url": "/mywallet/static/js/runtime-main.2be067d6.js"
+  },
+  {
+    "revision": "a56745839d90e961544b74ea8c99350d",
+    "url": "/mywallet/static/media/wallet.a5674583.png"
+  }
+]);
